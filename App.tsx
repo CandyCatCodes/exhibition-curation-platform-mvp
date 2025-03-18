38188b82595560a8d214e5a33e34a261439b6698
CandyCatCodes <candydavies22@gmail.com>
2025-03-18 02:16:50 +0000

    broaden LogBox ignore string for TNodeChildrenRenderer warning

--- App.tsx
+++ App.tsx
@@ -14,8 +14,9 @@ import { RootStackParamList } from './src/navigation/types';
 const Stack = createStackNavigator<RootStackParamList>();
 
 // Ignore the specific defaultProps warning from TNodeChildrenRenderer
+// Use a slightly broader match in case the exact string varies
 LogBox.ignoreLogs([
-  'Warning: TNodeChildrenRenderer: Support for defaultProps will be removed',
+  'TNodeChildrenRenderer: Support for defaultProps will be removed',
 ]);
 
 export default function App() {
