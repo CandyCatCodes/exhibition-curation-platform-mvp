9a8e1c9117243768c7c5a3c429cfcf23e83a89e2
CandyCatCodes <candydavies22@gmail.com>
2025-03-24 07:51:48 +0000

    clean up App.tsx code style

--- App.tsx
+++ App.tsx
@@ -1,44 +1,40 @@
-import 'react-native-gesture-handler'; // Must be at the top
-import React from 'react';
-import { LogBox } from 'react-native'; // Import LogBox
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar } from 'expo-status-bar';
-// Import the ExhibitionProvider
-import { ExhibitionProvider } from './src/context/ExhibitionContext';
-import HomeScreen from './src/screens/HomeScreen';
-import ArtworkDetailScreen from './src/screens/ArtworkDetailScreen';
-import ExhibitionScreen from './src/screens/ExhibitionScreen';
-import { RootStackParamList } from './src/navigation/types';
+import "react-native-gesture-handler";
+import React from "react";
+import { LogBox } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+import { ExhibitionProvider } from "./src/context/ExhibitionContext";
+import HomeScreen from "./src/screens/HomeScreen";
+import ArtworkDetailScreen from "./src/screens/ArtworkDetailScreen";
+import ExhibitionScreen from "./src/screens/ExhibitionScreen";
+import { RootStackParamList } from "./src/navigation/types";
 
 const Stack = createStackNavigator<RootStackParamList>();
 
-// Ignore the specific defaultProps warning from TNodeChildrenRenderer
-// Include the "Warning: " prefix as seen in the console output
 LogBox.ignoreLogs([
-  'Warning: TNodeChildrenRenderer: Support for defaultProps will be removed',
+  "Warning: TNodeChildrenRenderer: Support for defaultProps will be removed",
 ]);
 
 export default function App() {
   return (
-    // Wrap the entire navigation structure with the ExhibitionProvider
     <ExhibitionProvider>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen
             name="Home"
-          component={HomeScreen}
-          options={{ title: 'Artworks' }}
-        />
-        <Stack.Screen
-          name="ArtworkDetail"
-          component={ArtworkDetailScreen}
-          options={{ title: 'Artwork Details' }}
-        />
-        <Stack.Screen
-          name="Exhibition"
-          component={ExhibitionScreen}
-          options={{ title: 'My Exhibition' }}
+            component={HomeScreen}
+            options={{ title: "Artworks" }}
+          />
+          <Stack.Screen
+            name="ArtworkDetail"
+            component={ArtworkDetailScreen}
+            options={{ title: "Artwork Details" }}
+          />
+          <Stack.Screen
+            name="Exhibition"
+            component={ExhibitionScreen}
+            options={{ title: "My Exhibition" }}
           />
         </Stack.Navigator>
         <StatusBar style="auto" />
@@ -46,5 +42,3 @@ export default function App() {
     </ExhibitionProvider>
   );
 }
-
-// No need for the StyleSheet here anymore unless you add global styles later
